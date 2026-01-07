c205a5061411bbf4dc2ae9734b6992367c01074d
241199-code <241199@unamba.edu.pe>
2026-01-06 21:26:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
+const PORT = process.env.PORT || 10000;
 
-const PORT = process.env.PORT || 3000;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
-app.get("/", (req, res) => {
-  res.send("🚀 Hosting GRATIS en Render funcionando");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT now()");
+    res.send(`
+      <h1>✅ Render + CockroachDB funcionando</h1>
+      <p>Hora BD:</p>
+      <pre>${result.rows[0].now}</pre>
+    `);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 app.listen(PORT, () => {
-  console.log("Servidor activo en el puerto " + PORT);
+  console.log(`Servidor activo en el puerto ${PORT}`);
 });
